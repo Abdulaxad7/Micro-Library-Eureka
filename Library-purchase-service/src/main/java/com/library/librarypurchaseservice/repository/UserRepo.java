730a30111f716f7d6fccabe6574c7e0d9437247f
Abdulaxad7 <145189911+Abdulaxad7@users.noreply.github.com>
2024-06-21 20:00:20 +0500

    Delete Library-purchase-service directory

--- Library-purchase-service/src/main/java/com/library/librarypurchaseservice/repository/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.library.librarypurchaseservice.repository;
-
-import com.library.librarypurchaseservice.model.Books;
-import com.library.librarypurchaseservice.model.UserData;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepo extends MongoRepository<UserData,String> {
-
-}
